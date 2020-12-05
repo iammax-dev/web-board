c07615673ff4d007322493f8e4827130f9817e57
iammax-dev <iammax.dev@gmail.com>
2020-12-05 22:40:20 +0900

    201205 Google Login 연동, Security 설정

--- src/main/java/com/max/board/web/controller/IndexController.java
+++ src/main/java/com/max/board/web/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.max.board.web.controller;
 
+import com.max.board.web.config.auth.LoginUser;
+import com.max.board.web.config.auth.dto.SessionUser;
+import com.max.board.web.domain.user.User;
 import com.max.board.web.dto.PostsResponseDto;
 import com.max.board.web.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -8,24 +11,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 // 페이지 맵핑하는 컨트롤러
 public class IndexController {
 
     private final PostsService postsService;
+//    private final HttpSession  httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
-
+    public String index(Model model, @LoginUser SessionUser user) {
+        // postService 의 전체조회 메소드로 전체 목록 불러옴
         model.addAttribute("posts", postsService.findAllDesc());
+        
+        // 로그인 된 사용자 정보를 가져옴
+        // SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            // 사용자 정보가 있다면, 이름을 memberName 라는 attribute 로 추가해 view 로 던져줌
+            model.addAttribute("memberName", user.getName());
+        }
 
         // 머스태치 스타터때문에 앞 경로(src/main/resources/templates)와 뒷경로(.mustache) 추가됨
         return "index";
     }
 
     @GetMapping("/posts/save")
-    public String postsSave() {
+    public String postsSave(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("memberName", user.getName());
         return "posts-save";
     }
 
--- src/main/java/com/max/board/web/service/posts/PostsService.java
+++ src/main/java/com/max/board/web/service/posts/PostsService.java
@@ -19,6 +19,19 @@ public class PostsService {
 
     private final PostsRepository postsRepository;
 
+    public PostsResponseDto findById (Long id){
+
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+
+    }
+
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
@@ -38,19 +51,6 @@ public class PostsService {
 
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         postsRepository.delete(posts);
-
-    }
-
-    public PostsResponseDto findById (Long id){
-
-        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
-        return new PostsResponseDto(entity);
-    }
-
-    @Transactional(readOnly = true)
-    public List<PostsListResponseDto> findAllDesc() {
-
-        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
 
     }
 
