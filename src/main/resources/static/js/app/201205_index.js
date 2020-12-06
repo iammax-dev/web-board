fe6a2bd3f152834e57ae76c1eb6c4d3c1dbaeead
iammax-dev <iammax.dev@gmail.com>
2020-12-06 22:30:21 +0900

    201206 템플릿 엔진 thymeleaf 변경, 글 수정/삭제 권한 변경

--- src/main/resources/static/js/app/201205_index.js
+++ src/main/resources/static/js/app/201205_index.js
@@ -2,16 +2,36 @@ var main = {
     init : function(){
         var _this = this;
 
-        $('#btn-submit').on('click', function() {
+        $('#btn-save').on('click', function() {
+            _this.save();
+        });
+
+        $('#btn-update').on('click', function() {
             _this.update();
         });
 
         $('#btn-delete').on('click', function() {
             _this.delete();
         });
+    },
+    save : function() {
+        var data = {
+            title   : $('#title').val(),
+            author  : $('#author').val(),
+            content : $('#content').val()
+        };
 
-        $("#btn-cancel").click(function() {
-            document.location.href = "/";
+        $.ajax({
+            type        : 'POST',
+            url         : '/api/v1/posts',
+            dataType    : 'json',
+            contentType : 'application/json; charset=utf-8',
+            data        : JSON.stringify(data)
+        }).done(function () {
+            alert("글이 등록 되었습니다.");
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
         });
     },
     update : function() {
