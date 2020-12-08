1d705b62ed90b8d42d7d9fc6b7705e30ef6d058c
iammax-dev <iammax.dev@gmail.com>
2020-12-08 22:37:05 +0900

    201208 레이아웃 변경, css 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -10,31 +10,3 @@ $(document).ready(function(){
     });
 
 });
-
-
-//function fn_search() {
-//
-//    var selectSearch = $("#selectSearch option:selected").val();
-//    var keyword      = $('#keyword').val();
-//    var data = {
-//        selectSearch : selectSearch,
-//        keyword      : keyword
-//    };
-//
-//console.log(data)
-//jQuery.ajaxSettings.traditional = true;
-//    $.ajax({
-//        type        : 'GET',
-//        url         : '/search',
-////        dataType    : 'json',
-////        contentType : 'application/json; charset=utf-8',
-//        data        : data,
-//        success: function () {
-//            alert("검색성공.");
-//            document.location.href = "/search?selectSearch=" selectSearch + "&keyword=" + keyword
-//        },
-//        error: function (error) {
-//            alert(JSON.stringify(error));
-//        }
-//    });
-//}
